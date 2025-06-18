10656accde3917732c2b8d1270345c49e2cdbe27
sandhya-2250 <shresthasandhya225@gmail.com>
2025-06-18 10:45:14 +0545

    Update worksheet12.js

--- worksheet12.js
+++ worksheet12.js
@@ -1,6 +1,6 @@
 //Worksheet 12: Local Storage
 
 
-localStorage.setItem("username", "Sandesh");
+localStorage.setItem("username", "Sandhya");
 let user = localStorage.getItem("username");
 console.log(user);
