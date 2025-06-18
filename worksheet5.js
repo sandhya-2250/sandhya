a975fd7344e4682b6fec1d391354e07755a7ac82
sandhya-2250 <shresthasandhya225@gmail.com>
2025-06-18 12:53:23 +0545

    Update worksheet5.js

--- worksheet5.js
+++ worksheet5.js
@@ -2,4 +2,4 @@
 function sandhya(name, cast, age, school) {
   console.log("Hello, " + name + " " + cast + " " + age + " " + school + "!");
 }
-sandhya("sandhya\t", "Shrestha\n", "17\n", "Adarsha Secondary School");
+sandhya("sandhya\t", "Shrestha\n", "18\n", "Adarsha Secondary School");
