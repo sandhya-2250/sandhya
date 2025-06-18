e119b1593a879fa927e7aecb079d347a7f632477
sandhya-2250 <shresthasandhya225@gmail.com>
2025-06-18 12:49:16 +0545

    Update worksheet6.js

--- worksheet6.js
+++ worksheet6.js
@@ -1,15 +1,15 @@
 //worksheet6.js array
 
 let students=[
-  {name:"Sandesh",cast:"Ghimire",age:17},
-  {name:"Falana",cast:"Diskana",age:"Who knows"},
-  {name:"Diskana",cast:"Falana",age:"Who knows"},
+  {name:"Sandhya",cast:"Shrestha",age:17},
+  {name:"Kripa",cast:"Shrestha",age:"Who knows"},
+  {name:"Dipsana",cast:"Karki",age:"Who knows"},
   ];
   
     console.log(students[0].name);
     console.log(students[0].cast);
     console.log(students[0].age);
 //output:
-// Sandesh
-// Ghimire
+// Sandhya
+// Shrestha
 // 17
