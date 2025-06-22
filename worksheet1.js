23c7814cc63d66786fb2c5aa3265aff1c258973e
sandhya-2250 <shresthasandhya225@gmail.com>
2025-06-22 19:00:46 +0545

    worksheet

--- worksheet1.js
+++ worksheet1.js
@@ -3,14 +3,14 @@
 // This is a simple JavaScript program that declares three variables
 // and prints their values to the console.
 let name = "sandhya";
-let age = 17;
+let age = 18;
 let student = false;
 
 console.log(name);
 console.log(age);
 console.log(student);
 /*output:
-sandesh
-17
+sandhya
+18
 false
 */
